e1234dcbf9f91442852ff711beae7397be167d83
th14g02010 <140118003+th14g02010@users.noreply.github.com>
2025-04-09 13:45:49 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import time
+import json
+import requests
+from google.oauth2 import service_account
+import google.auth.transport.requests
+
+SERVICE_ACCOUNT_FILE = "cryptojaspion-3859411c1c08.json"
+TARGET = "/topics/all"
+
+credentials = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE,
+    scopes=["https://www.googleapis.com/auth/firebase.messaging"]
+)
+
+def get_access_token():
+    request = google.auth.transport.requests.Request()
+    credentials.refresh(request)
+    return credentials.token
+
+def send_push_notification(title, body):
+    access_token = get_access_token()
+    url = "https://fcm.googleapis.com/v1/projects/cryptojaspion/messages:send"
+
+    message = {
+        "message": {
+            "topic": "all",
+            "notification": {
+                "title": title,
+                "body": body
+            }
+        }
+    }
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json; UTF-8",
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(message))
+    print(f"Status: {response.status_code}, Response: {response.text}")
+
+if __name__ == "__main__":
+    while True:
+        send_push_notification("Alerta Crypto", "Bitcoin está próximo do ATH!")
+        time.sleep(3600)
